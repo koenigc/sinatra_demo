d1ddbc600391871431820773eaf17cac6265fa1a
koenigc <christoph.koenig@sales-lentz.lu>
2009-08-04 10:07:26 +0200

    Sinatra App ersteelt. Ausführen mit "ruby myapp.rb"

---
+++ myapp.rb
@@ -0,0 +1,37 @@
+# Einbinden was benötigt wird
+require 'rubygems'
+require 'haml'
+require 'sass'
+require 'sinatra'
+require 'sinatra/r18n'
+
+
+# KONFIGURATION
+enable  :sessions
+# set :haml, {:format => :html5 } # default Haml format is :xhtml
+# Hier muss ich noch nach den &nbsp; schauen, da der Validator fehler ausgibt. Daher erstmal nur normales (x)html
+
+
+# Locale wenn vorhanden in die Session schreiben (?locale=xx)
+before do
+  session[:locale] = params[:locale] if params[:locale]
+end
+
+
+
+get '/' do
+  haml :index
+end
+
+get '/test' do
+  haml :test
+end
+
+
+
+
+# SASS Stylesheet einbinden
+get '/stylesheet.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :stylesheet
+end
